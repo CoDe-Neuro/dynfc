b2faab6dbc4bc6c7758bbb2cf1fba1f0ddcd476d
lucasfr <lucas.franca.14@ucl.ac.uk>
2021-01-31 16:08:01 +0000

    :rotating_light: Test framework + first tests

--- tests/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python -m pytest ./tests/test_functions.py
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,7 +1,11 @@
 import dynfc as dyn
 from numpy import pi
 
-def test_phDiff():
-    assert dyn.phDiff(pi, pi) == 1
-    #assert dyn.phDiff(pi, pi/2) == 0 
-    #assert dyn.phDiff(pi, 0) == - 1
+def test_phDiff1():
+    assert round(dyn.phDiff(pi, pi),1) == 1
+
+def test_phDiff2():
+    assert round(dyn.phDiff(pi, pi/2), 1) == 0
+
+def test_phDiff3():
+    assert round(dyn.phDiff(pi, 0),1) == - 1
