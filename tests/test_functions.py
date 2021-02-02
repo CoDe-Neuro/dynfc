46bc4a843bad8b1b582ff4d8a76559bc6e1b01ee
lucasfr <lucas.franca.14@ucl.ac.uk>
2021-02-02 12:54:46 +0000

    :rotating_light: New tests

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,5 +1,6 @@
 import dynfc as dyn
 from numpy import pi
+from numpy.random import rand, seed
 
 def test_phDiff1():
     assert round(dyn.phDiff(pi, pi),1) == 1
@@ -9,3 +10,13 @@ def test_phDiff2():
 
 def test_phDiff3():
     assert round(dyn.phDiff(pi, 0),1) == - 1
+
+def test_cofluct1():
+    seed(42)
+    rand_vec = rand(1, 1024)
+    assert round(dyn.cofluct(rand_vec, rand_vec)) == 1
+
+def test_cofluct2():
+    seed(42)
+    rand_vec = rand(1, 1024)
+    assert round(dyn.cofluct(rand_vec, -rand_vec)) == -1
