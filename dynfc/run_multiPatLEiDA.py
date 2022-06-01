46ffeeab9e19ea169d6993eca03ddb6411f826e4
lucasfr <lucas.franca@kcl.ac.uk>
2022-06-01 16:27:55 +0100

    :sparkles: Adding filter params to function

--- dynfc/run_multiPatLEiDA.py
+++ dynfc/run_multiPatLEiDA.py
@@ -1,16 +1,19 @@
 from numpy import zeros
 from .butter_bandpass_filter import butter_bandpass_filter
-from .doKuramoto import doKuramoto
 from .doHilbert import doHilbert
 from .dPL import dPL
 
 
 
-def run_multiPatLEiDA(RSsig):
+def run_multiPatLEiDA(RSsig, delt, flp, fhi, k):
     """Run LEiDA Routine for BOLD signal.
 
     Args:
         RSsig (ndarray): BOLD signal array for all parcels/voxels in the format [N, Tmax, Subs].
+        delt: TR sample.
+        flp: low threshold frequency of filter
+        fhi: high threshold frequency of filter
+        k: filter order
 
     Returns:
         tuple:
@@ -21,17 +24,17 @@ def run_multiPatLEiDA(RSsig):
     References
     ----------
 
-    .. [1] 
-    Cabral, J. et al. (2017) ‘Cognitive performance in healthy 
-    older adults relates to spontaneous switching between states 
-    of functional connectivity during rest’, Scientific Reports. 
-    Nature Publishing Group, 7(1), p. 5135. 
+    .. [1]
+    Cabral, J. et al. (2017) ‘Cognitive performance in healthy
+    older adults relates to spontaneous switching between states
+    of functional connectivity during rest’, Scientific Reports.
+    Nature Publishing Group, 7(1), p. 5135.
     doi: 10.1038/s41598-017-05425-7.
 
-    .. [2] 
-    Lord et al,. (2019). Dynamical exploration of the 
-    repertoire of brain networks at rest is 
-    modulated by psilocybin. NeuroImage, 199(April), 127–142. 
+    .. [2]
+    Lord et al,. (2019). Dynamical exploration of the
+    repertoire of brain networks at rest is
+    modulated by psilocybin. NeuroImage, 199(April), 127–142.
     https://doi.org/10.1016/j.neuroimage.2019.05.060
 
 
@@ -45,11 +48,11 @@ def run_multiPatLEiDA(RSsig):
     syncConn = zeros([N, N, Tmax - 20, nSub])
     phases = zeros([N, Tmax, nSub])
 
-    flp = .04              # lowpass frequency of filter
-    fhi = .07              # highpass
+    #flp = .04              # lowpass frequency of filter
+    #fhi = .07              # highpass
     npts = Tmax            # total nb of points
-    delt = 2               # sampling interval
-    k = 2                  # 2nd order butterworth filter
+    #delt = 0.392               # sampling interval
+    #k = 2                  # 2nd order butterworth filter
 
     for pat in range(nSub):
 
@@ -69,5 +72,5 @@ def run_multiPatLEiDA(RSsig):
 
         print('Matrices obtained.')
         print('Routine finished for patient no. ' + str(pat + 1) + '.')
-    
+
     return phases, syncConn, leidaArray
