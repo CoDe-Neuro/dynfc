34a88920ecfae5af2803b483a2b97ba3dc2795fb
Lucas França <lucas.franca@kcl.ac.uk>
2022-06-06 01:13:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="dynfc", 
-    version="0.0.2-a1",
+    version="0.0.3-a1",
     author="Lucas G. S. França, Dafnis Batalle",
     author_email="lucas.franca@kcl.ac.uk, dafnis.batalle@kcl.ac.uk",
     description="",
